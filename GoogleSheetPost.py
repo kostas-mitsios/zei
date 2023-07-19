a5ad894b75e2df53a6cc46f61b567c5808518268
Kostas <rotundjere@hotmail.com>
2023-07-19 15:02:38 +0300

    Added files

---
+++ GoogleSheetPost.py
@@ -0,0 +1,25 @@
+import os
+import gspread
+from google.oauth2 import service_account
+
+# Replace 'path/to/your/credentials.json' with the actual path to your credentials file.
+credentials = service_account.Credentials.from_service_account_file('C:/Users/kmitsios/Downloads/credentials.json')
+client = gspread.authorize(credentials)
+
+# Replace '1sm_ePXHiTxty5gHfgW_rWGhzdnml1G6sVfuwKfKWeMM' with your actual Google Sheet ID.
+sheet_id = '1sm_ePXHiTxty5gHfgW_rWGhzdnml1G6sVfuwKfKWeMM'
+# Replace 'Dogs' with the name of the specific sheet you want to post data to.
+sheet_name = 'Dogs'
+sheet = client.open_by_key(sheet_id).worksheet(sheet_name)
+
+# Replace data_list with the list of data you want to post to the sheet.
+# Each element in data_list represents a row of data in the sheet.
+data_list = [
+    ['Buddy', 5, 'Labrador'],
+    ['Max', 3, 'Golden Retriever'],
+    ['Lucy', 2, 'Poodle'],
+]
+
+# Update the data in the specific range (A14:C16).
+# This assumes that your data_list has exactly three rows (3 elements).
+sheet.update('A14:C16', data_list)
---
+++ GoogleSheetSearch.py
@@ -0,0 +1,37 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import requests
+
+# Replace with your own credentials file path
+credentials_file = 'C:/Users/kmitsios/Downloads/credentials.json'
+api_key = 'AIzaSyDowT_pE8qSIUtJrTa7AsBWj6X2wR8migw'
+
+# Create a custom session with the API key
+session = requests.Session()
+session.headers.update({'Authorization': f'Bearer {api_key}'})
+
+# Authenticate with Google Sheets API using the custom session
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_name(credentials_file, scope)
+client = gspread.Client(auth=credentials, session=session)
+
+# Open the Google Sheet by its ID
+sheet_id = "1sm_ePXHiTxty5gHfgW_rWGhzdnml1G6sVfuwKfKWeMM"
+sheet = client.open_by_key(sheet_id)
+
+# Specify the named range
+named_range = "dogs"
+
+# Get all values from the named range
+range_data = sheet.values_get(named_range)
+
+if "values" in range_data:
+    # Search for the value "aaa" in column A and retrieve all other data from the same row
+    search_value = "aaa"
+    for row_values in range_data["values"]:
+        if row_values[0] == search_value:  # Check if the value is found in column A
+            print("Data from the row containing 'aaa':", row_values)
+            break  # Stop searching after the first match
+else:
+    print("Named range 'dogs' not found.")
