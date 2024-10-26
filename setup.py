c2e04a9257fc6b8f9c038ec8cdbd5a940e11838e
Your Name <rotundjere@hotmail.com>
2024-10-26 13:30:06 +0300

    Moved irrelevant files

--- setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name="boss_music",
-    version="1.0",
-    description="Your application description",
-    executables=[Executable("boss_music.py")]
-)
